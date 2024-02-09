5cf4f3b1995d9711001b05f9caa93452ba188d3f
Wisit018 <lipton_pink@hotmail.co.th>
2024-02-09 17:29:58 +0700

    update2

--- React-Leaen-Start/reate-basic-1/src/App.js
+++ React-Leaen-Start/reate-basic-1/src/App.js
@@ -7,13 +7,23 @@ function App() {
     { id: 3, name: "CCC" },
     { id: 4, name: "CCC" },
   ]);
+
+  function deleteStudent(id) {
+    console.log(id);
+  }
+
   //พื้นที่แสดงผล
   return (
     <>
       <h1>จำนวนนักเรียน : {students.length}</h1>
       <ul>
         {students.map((item) => (
-          <li key={item.id}>{item.id} - {item.name}</li>
+          <li key={item.id}>
+            <p>{item.id} - {item.name}</p>
+            <button onClick={()=>deleteStudent(item.id)}>ลบ</button>
+
+          </li>
+          
         ))}
       </ul>
     </>
